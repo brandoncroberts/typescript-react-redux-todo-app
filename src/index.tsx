04ddffb50d093636b77fae6aaff7d020c1a0a411
brandon roberts <8brandon@protonmail.com>
2020-04-13 15:53:21 +0200

    Make counter app

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component {
+interface AppProps {
+  color?: string;
+}
+
+class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+
+  onIcrement = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div>Hey</div>;
+    return (
+      <div>
+        <button onClick={this.onIcrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
