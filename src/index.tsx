1292c62027409e55c281e0eda078b4e848ddeaa6
brandon roberts <8brandon@protonmail.com>
2020-04-13 19:20:42 +0200

    Install redux and other dependencies. Initalize redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import App from "./componenets/App";
+import { reducers } from "./reducers/index";
 
-interface AppProps {
-  color?: string;
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
-interface AppState {
-  counter: number;
-}
-
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    this.state = { counter: 0 };
-  }
-
-  onIcrement = () => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  onDecrement = () => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIcrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
